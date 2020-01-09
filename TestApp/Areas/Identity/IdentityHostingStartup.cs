7f28a8cf28488793055777ac27f77db0e81d79d0
Gaelian Ditchburn <gaelian.ditchburn@ag.gov.au>
2020-01-10 10:55:25 +1100

    Initial commit.

---
+++ TestApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestApp.Areas.Identity.Data;
+using TestApp.Models;
+
+[assembly: HostingStartup(typeof(TestApp.Areas.Identity.IdentityHostingStartup))]
+namespace TestApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TestAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TestAppContextConnection")));
+
+                services.AddDefaultIdentity<TestAppUser>()
+                    .AddEntityFrameworkStores<TestAppContext>();
+            });
+        }
+    }
+}
